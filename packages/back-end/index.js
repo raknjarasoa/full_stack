6e4abd8746b8774ce23b9892c2f91c147743f081
Njarasoa Rakotozafy <raknjarasoa@gmail.com>
2019-12-03 14:30:51 +0100

    Add server for heroku CD

---
+++ packages/back-end/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const compression = require('compression')
+const path = require('path');
+const PORT = process.env.PORT || 8081;
+
+const app = express();
+
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, '../front-end/dist/front-end/')));
+
+app.get('/*', function (req, res) {
+  console.log(Date.now())
+  res.sendFile(path.join(__dirname, '../front-end/dist/front-end/index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
